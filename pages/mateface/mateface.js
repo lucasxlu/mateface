cb96d7d398dca89a6a1cd8b35635610c908e4b35
EclipseXuLu <249048056@qq.com>
2018-07-21 21:07:36 +0800

    update

--- pages/mateface/mateface.js
+++ pages/mateface/mateface.js
@@ -1,10 +1,11 @@
 // pages/mateface.js
-const util = require('../../utils/util.js')
+const util = require('../../utils/util.js'), json0 = { "image_id": "O2alrpeRIXFejHWe6WlRqw==", "request_id": "1532177254,0a68158a-c578-4877-8f4e-bfa96ba8b847", "time_used": 1100, "faces": [{ "landmark": { "mouth_upper_lip_left_contour2": { "y": 489, "x": 520 }, "mouth_upper_lip_top": { "y": 482, "x": 530 }, "mouth_upper_lip_left_contour1": { "y": 481, "x": 524 }, "left_eye_upper_left_quarter": { "y": 412, "x": 512 }, "left_eyebrow_lower_middle": { "y": 394, "x": 513 }, "mouth_upper_lip_left_contour3": { "y": 493, "x": 525 }, "right_eye_top": { "y": 404, "x": 579 }, "left_eye_bottom": { "y": 420, "x": 519 }, "right_eyebrow_lower_left_quarter": { "y": 392, "x": 568 }, "right_eye_pupil": { "y": 409, "x": 578 }, "mouth_lower_lip_right_contour1": { "y": 495, "x": 549 }, "mouth_lower_lip_right_contour3": { "y": 505, "x": 545 }, "mouth_lower_lip_right_contour2": { "y": 502, "x": 556 }, "contour_chin": { "y": 544, "x": 537 }, "contour_left9": { "y": 536, "x": 524 }, "left_eye_lower_right_quarter": { "y": 418, "x": 525 }, "mouth_lower_lip_top": { "y": 495, "x": 533 }, "right_eyebrow_upper_middle": { "y": 380, "x": 582 }, "left_eyebrow_left_corner": { "y": 396, "x": 501 }, "right_eye_bottom": { "y": 417, "x": 581 }, "contour_left7": { "y": 508, "x": 513 }, "contour_left6": { "y": 494, "x": 510 }, "contour_left5": { "y": 479, "x": 508 }, "contour_left4": { "y": 464, "x": 506 }, "contour_left3": { "y": 450, "x": 506 }, "contour_left2": { "y": 436, "x": 508 }, "contour_left1": { "y": 422, "x": 509 }, "left_eye_lower_left_quarter": { "y": 419, "x": 513 }, "contour_right1": { "y": 416, "x": 665 }, "contour_right3": { "y": 461, "x": 664 }, "contour_right2": { "y": 438, "x": 665 }, "mouth_left_corner": { "y": 499, "x": 522 }, "contour_right4": { "y": 484, "x": 658 }, "contour_right7": { "y": 531, "x": 609 }, "right_eyebrow_left_corner": { "y": 392, "x": 553 }, "nose_right": { "y": 460, "x": 554 }, "nose_tip": { "y": 446, "x": 518 }, "contour_right5": { "y": 504, "x": 648 }, "nose_contour_lower_middle": { "y": 464, "x": 526 }, "left_eyebrow_lower_left_quarter": { "y": 394, "x": 506 }, "mouth_lower_lip_left_contour3": { "y": 506, "x": 527 }, "right_eye_right_corner": { "y": 411, "x": 597 }, "right_eye_lower_right_quarter": { "y": 415, "x": 590 }, "mouth_upper_lip_right_contour2": { "y": 487, "x": 552 }, "right_eyebrow_lower_right_quarter": { "y": 391, "x": 597 }, "left_eye_left_corner": { "y": 416, "x": 509 }, "mouth_right_corner": { "y": 496, "x": 566 }, "mouth_upper_lip_right_contour3": { "y": 491, "x": 548 }, "right_eye_lower_left_quarter": { "y": 416, "x": 572 }, "left_eyebrow_right_corner": { "y": 396, "x": 529 }, "left_eyebrow_lower_right_quarter": { "y": 396, "x": 521 }, "right_eye_center": { "y": 411, "x": 580 }, "nose_left": { "y": 453, "x": 511 }, "mouth_lower_lip_left_contour1": { "y": 496, "x": 527 }, "left_eye_upper_right_quarter": { "y": 411, "x": 525 }, "right_eyebrow_lower_middle": { "y": 390, "x": 583 }, "left_eye_top": { "y": 410, "x": 518 }, "left_eye_center": { "y": 415, "x": 519 }, "contour_left8": { "y": 522, "x": 518 }, "contour_right9": { "y": 545, "x": 561 }, "right_eye_left_corner": { "y": 414, "x": 564 }, "mouth_lower_lip_bottom": { "y": 507, "x": 534 }, "left_eyebrow_upper_left_quarter": { "y": 388, "x": 505 }, "left_eye_pupil": { "y": 414, "x": 519 }, "right_eyebrow_upper_left_quarter": { "y": 383, "x": 566 }, "contour_right8": { "y": 539, "x": 586 }, "right_eyebrow_right_corner": { "y": 393, "x": 613 }, "right_eye_upper_left_quarter": { "y": 407, "x": 570 }, "left_eyebrow_upper_middle": { "y": 386, "x": 513 }, "right_eyebrow_upper_right_quarter": { "y": 383, "x": 599 }, "nose_contour_left1": { "y": 414, "x": 528 }, "nose_contour_left2": { "y": 440, "x": 516 }, "mouth_upper_lip_right_contour1": { "y": 480, "x": 536 }, "nose_contour_right1": { "y": 414, "x": 552 }, "nose_contour_right2": { "y": 444, "x": 550 }, "mouth_lower_lip_left_contour2": { "y": 502, "x": 524 }, "contour_right6": { "y": 519, "x": 631 }, "nose_contour_right3": { "y": 464, "x": 540 }, "nose_contour_left3": { "y": 460, "x": 517 }, "left_eye_right_corner": { "y": 415, "x": 530 }, "left_eyebrow_upper_right_quarter": { "y": 389, "x": 522 }, "right_eye_upper_right_quarter": { "y": 406, "x": 589 }, "mouth_upper_lip_bottom": { "y": 490, "x": 532 } }, "attributes": { "emotion": { "sadness": 0.385, "neutral": 73.459, "disgust": 0.004, "anger": 0.004, "surprise": 25.761, "fear": 0.004, "happiness": 0.381 }, "gender": { "value": "Female" }, "age": { "value": 24 }, "eyestatus": { "left_eye_status": { "normal_glass_eye_open": 0.029, "no_glass_eye_close": 0.0, "occlusion": 0.087, "no_glass_eye_open": 99.88, "normal_glass_eye_close": 0.0, "dark_glasses": 0.005 }, "right_eye_status": { "normal_glass_eye_open": 0.057, "no_glass_eye_close": 0.0, "occlusion": 0.007, "no_glass_eye_open": 99.932, "normal_glass_eye_close": 0.0, "dark_glasses": 0.003 } }, "glass": { "value": "None" }, "headpose": { "yaw_angle": 52.055008, "pitch_angle": -9.8546915, "roll_angle": 6.9750133 }, "blur": { "blurness": { "threshold": 50.0, "value": 0.036 }, "motionblur": { "threshold": 50.0, "value": 0.036 }, "gaussianblur": { "threshold": 50.0, "value": 0.036 } }, "smile": { "threshold": 50.0, "value": 0.104 }, "facequality": { "threshold": 70.1, "value": 0.006 }, "ethnicity": { "value": "WHITE" } }, "face_rectangle": { "width": 162, "top": 379, "left": 506, "height": 162 }, "face_token": "153c824deb95b857aec38f0d34b49123" }] };
 var appInstance = getApp()
 Page({
   data: {
-    files: [],
-    ifshow:''
+    files: ['../../source/images/detect/pic00.jpg', '../../source/images/detect/pic01.png'],
+    ifshow:'hide',
+    result: util.formatJson(json0)
   },
   chooseImage: function (e) {
     var that = this;
@@ -13,6 +14,8 @@ Page({
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
+       
+        that.data.files=[];
         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
         var json = {
           "status": 0,
@@ -27,10 +30,9 @@ Page({
         that.setData({
           files: that.data.files.concat(res.tempFilePaths),
           ifshow:'hide',
-          result: util.formatJson(json),
-          result1: "{\n'image_id': 'O2alrpeRIXFejHWe6WlRqw==',\n'request_id':'153217279559f90561-5a30-4b8b-ba25-22a306471b7a',\n'time_used': 1032,\n'faces':[{\n'attributes': {\n'gender': {'value': 'Female'\n },\n'beauty': {\nfemale_score': 77.869,\n'male_score': 75.949\n}\n},\n'face_rectangle': {\n'width': 162,\n'top': 379,\n'left': 506,\n'height': 162\n},\n'face_token': '5d25ecc34711ec1dc8913ebf295e27f4'\n}\n]\n}"       
+          result: util.formatJson(json)      
         });
-
+        console.log(that.data.files)
         console.log(res.tempFilePaths)
         // wx.showToast({
         //   icon: "loading",
@@ -58,11 +60,14 @@ Page({
     })
   },
   previewImage: function (e) {
-    console.log(e)
-    wx.previewImage({
-      current: e.currentTarget.id, // 当前显示图片的http链接
-      urls: this.data.files // 需要预览的图片http链接列表
-    })
+    if (e.currentTarget.dataset.index==0){
+      this.chooseImage();
+    }else{
+      wx.previewImage({
+        current: e.currentTarget.id, // 当前显示图片的http链接
+        urls: this.data.files // 需要预览的图片http链接列表
+      })
+    }
   },
 
   /**
